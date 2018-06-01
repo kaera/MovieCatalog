94d88e160dc3ab8ea365fd78672bfef8761d4275
kaera <anastasy.d@gmail.com>
2018-06-01 23:28:36 +0200

    Add web-server

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import data from '../../data.json';
 
 class HomePage extends Component {
     constructor() {
         super();
+        console.log(
+            data
+        )
     }
 
     render() {
