e297f10b15020f69126e5ec4097d783f13faf3d0
kaera <anastasy.d@gmail.com>
2018-06-02 10:56:08 +0200

    Fix MovieCard

--- src/js/components/MovieCard.js
+++ src/js/components/MovieCard.js
@@ -11,7 +11,17 @@ class MovieCard extends Component {
                 <img className="card-img-top" src={this.props.data.img} alt={this.props.data.title} />
                 <div className="card-body">
                     <h5 className="card-title">{this.props.data.title}</h5>
-                    <p className="card-text">{this.props.data.plot}</p>
+                    <p className="card-text">Год выпуска: {this.props.data.year}</p>
+                    <p className="card-text">
+                        {this.props.data.categories.title}:&nbsp;
+                        {this.props.data.categories.list.map(item => item.name).join(', ')}
+                    </p>
+                    <p className="card-text">
+                        {this.props.data.collectives[0].title}:&nbsp;
+                        {this.props.data.collectives[0].list.map(item => item.name).join(', ')}
+                    </p>
+                    <p className="card-text">Продолжительность: {this.props.data.duration}</p>
+
                     <a href={this.props.data.id} className="btn btn-link">Show details</a>
                 </div>
             </div>
