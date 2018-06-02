fcaedd2f76a2705516b52d66b1abe10aa4d9e797
kaera <anastasy.d@gmail.com>
2018-06-02 11:39:03 +0200

    Display movie properties

--- src/js/components/MoviePage.js
+++ src/js/components/MoviePage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import MovieCard from './MovieCard';
 
 class MoviePage extends Component {
     constructor(props) {
@@ -9,8 +8,36 @@ class MoviePage extends Component {
     render() {
         return (
             <div>
-                <h1>{this.props.data.title}</h1>
-                <MovieCard data={this.props.data} />
+                <div className="media">
+                    <img className="mr-3" src={this.props.data.img} alt={this.props.data.title} />
+                    <div className="media-body">
+                        <h2 className="mt-0">{this.props.data.title}</h2>
+                        {Object.entries(this.props.data.ratings).map(([name, rating]) => <p className="text-capitalize" key={name}>{name}: {rating}</p>)}
+                        <p>{this.props.data.plot}</p>
+
+                    </div>
+
+                </div>
+
+                <dl className="row mt-5">
+                    <dt className="col-sm-3">Год выпуска:</dt>
+                    <dd className="col-sm-9">{this.props.data.year}</dd>
+
+                    <dt className="col-sm-3">{this.props.data.categories.title}:</dt>
+                    <dd className="col-sm-9">{this.props.data.categories.list.map(item => item.name).join(', ')}</dd>
+
+                    <dt className="col-sm-3">{this.props.data.countries.title}:</dt>
+                    <dd className="col-sm-9">{this.props.data.countries.list.map(item => item.name).join(', ')}</dd>
+
+                    <dt className="col-sm-3">{this.props.data.collectives[0].title}:</dt>
+                    <dd className="col-sm-9">{this.props.data.collectives[0].list.map(item => item.name).join(', ')}</dd>
+
+                    <dt className="col-sm-3">{this.props.data.collectives[1].title}:</dt>
+                    <dd className="col-sm-9">{this.props.data.collectives[1].list.map(item => item.name).join(', ')}</dd>
+
+                    <dt className="col-sm-3">Продолжительность:</dt>
+                    <dd className="col-sm-9">{this.props.data.duration}</dd>
+                </dl>
             </div>
         )
     }
